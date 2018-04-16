ae0d0446c4016adede70ab688912040eea4f1fd4
JT <justin@justintaylor.net>
2018-04-16 16:55:13 +1000

    Clean up namespaces.

--- test/Sashay.Core.FakeFunctions/TestFunctions.cs
+++ test/Sashay.Core.FakeFunctions/TestFunctions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 
 namespace Sashay.Core.FakeFunctions
