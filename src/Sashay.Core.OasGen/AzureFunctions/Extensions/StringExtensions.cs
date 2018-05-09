4f38e5bdfd78ce63fcf4d806c43757726c088bda
JT <justin@justintaylor.net>
2018-05-09 11:12:32 +1000

    Added string extension to check whether a route string contains a parameter.

---
+++ src/Sashay.Core.OasGen/AzureFunctions/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Sashay.Core.OasGen.AzureFunctions.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool ContainsParameter(this string route, string parameterName)
+        {
+            if (route == null) throw new ArgumentNullException(nameof(route));
+            if (parameterName == null) throw new ArgumentNullException(nameof(parameterName));
+
+            var pattern = ".*{" + parameterName.Trim() + @"((:){1}(\w+))?}";
+
+            return Regex.IsMatch(route, pattern, RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ test/Sashay.Core.OasGen.Tests/AzureFunctions/StringExtensionTests/ContainsParameter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Sashay.Core.OasGen.AzureFunctions.Extensions;
+using Xunit;
+
+namespace Sashay.Core.OasGen.Tests.AzureFunctions.StringExtensionTests
+{
+    public class ContainsParameter
+    {
+        [Theory]
+        [InlineData("{parameter}")]
+        [InlineData("{myparameter")]
+        [InlineData("myparameter}")]
+        [InlineData("/route")]
+        [InlineData("/route/{differentParameter}/")]
+        [InlineData("{myparameteralpha]}")]
+        [InlineData("/{myparameteralpha}")]
+        public void WithoutParameterInString_ReturnsFalse(string route)
+        {
+            Assert.False(route.ContainsParameter("myParameter"));
+        }
+
+        [Fact]
+        public void WithNullString_ThrowsArgumentNullException()
+        {
+            string nullString = null;
+            Assert.Throws<ArgumentNullException>(() => nullString.ContainsParameter("myParameter"));
+        }
+
+        [Fact]
+        public void WithNullParameter_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => "someString".ContainsParameter(null));
+        }
+
+        [Theory]
+        [InlineData("/{myparameter}")]
+        [InlineData("/{myparameter:alpha}")]
+        [InlineData("/{myparameter:delta}")]
+        [InlineData("/{myparameter:beta}")]
+        [InlineData("/someRoute/{myparameter}")]
+        [InlineData("someRoute/{myparameter}")]
+        public void WithParameterInString_ReturnsTrue(string route)
+        {
+            Assert.True(route.ContainsParameter("myParameter"));
+        }
+    }
+}
